e6299ffafaa22775aab879a0d5ad64c00b79bfcf
Jessica Phoenix <jessica.phoenix30@gmail.com>
2023-07-22 16:57:37 -0400

    apiconnect

--- script.js
+++ script.js
@@ -1,27 +1,21 @@
-const factContainer = document.getElementById("fact-container");
-const apiUrl = "https://cat-fact.herokuapp.com/facts/random";
+const fetchButton = document.getElementById('fetchButton');
+const resultsContainer = document.getElementById('resultsContainer');
 
-function displayRandomCatFact() {
-  fetch(apiUrl)
-    .then((response) => {
-      if (!response.ok) {
-        throw new Error("Network response was not ok");
-      }
-      return response.json();
-    })
-    .then((data) => {
-      if (data.all && data.all.length > 0) {
-        const randomIndex = Math.floor(Math.random() * data.all.length);
-        const randomFact = data.all[randomIndex].text;
-        factContainer.innerHTML = `<p>${randomFact}</p>`;
-      } else {
-        factContainer.innerHTML = "<p>No cat facts found.</p>";
-      }
-    })
-    .catch((error) => {
-      factContainer.innerHTML = `<p>Error fetching cat facts: ${error.message}</p>`;
-    });
+fetchButton.addEventListener('click', fetchData);
+
+function fetchData() {
+    fetch('https://jsonplaceholder.typicode.com/posts')
+        .then(response => response.json())
+        .then(data => displayResults(data))
+        .catch(error => console.error('Error fetching data:', error));
 }
 
-// Display a random cat fact when the page loads
-displayRandomCatFact();
+function displayResults(data) {
+    resultsContainer.innerHTML = '';
+
+    data.forEach(post => {
+        const postElement = document.createElement('div');
+        postElement.innerHTML = `<h2>${post.title}</h2><p>${post.body}</p>`;
+        resultsContainer.appendChild(postElement);
+    });
+}
